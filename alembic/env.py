69b1f00ec6f2c0a102cf45e05cd05765bdbf241b
Sluicee <sluicee1@gmail.com>
2025-12-04 22:16:10 +0400

    feat: Initialize Alembic for database migrations and add an example environment configuration.

--- alembic/env.py
+++ alembic/env.py
@@ -20,11 +20,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.db.session import settings
-# You would import your Base here, e.g.:
-# from app.db.base import Base
-# target_metadata = Base.metadata
-target_metadata = None
+from app.db.base import Base
+from app.core.config import settings
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
