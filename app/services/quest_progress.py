710971d0d298953662e98177cf40f69b556b4fa9
Sluicee <sluicee1@gmail.com>
2025-12-28 18:08:37 +0400

    fix: lint error (Undefined name 'selectinload')

--- app/services/quest_progress.py
+++ app/services/quest_progress.py
@@ -6,6 +6,7 @@ from uuid import UUID
 from datetime import datetime, date, timezone
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, and_, func, delete
+from sqlalchemy.orm import selectinload
 import random
 import logging
 
